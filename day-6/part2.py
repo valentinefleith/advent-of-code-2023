602fe768f8cbf4f3529d46215a0cfbfdd30a9fdb
valentinefleith <valentine.fleith@gmail.com>
2023-12-06 10:29:56 +0100

    import class instead of rewriting it

--- day-6/part2.py
+++ day-6/part2.py
@@ -1,6 +1,8 @@
 from sys import argv, exit
 from typing import List
 
+from part1 import Race
+
 
 def main():
     if len(argv) != 2:
@@ -8,20 +10,6 @@ def main():
     race = parse_file(argv[1])
     numbers_of_ways_to_win = len(race.get_all_ways_to_win())
     print(numbers_of_ways_to_win)
-
-
-class Race:
-    def __init__(self, time, record):
-        self.time = time
-        self.record = record
-
-    def get_all_ways_to_win(self):
-        holding_times = []
-        for holding_time in range(1, self.time):
-            travelled_distance = holding_time * (self.time - holding_time)
-            if travelled_distance > self.record:
-                holding_times.append(holding_time)
-        return holding_times
 
 
 def parse_file(path):
